798543cba5799c436d77766d65fec5e798848c43
Emily Wolchko <e.wolchko@gmail.com>
2020-10-01 16:54:41 -0400

    Updating index and php files

--- p1/index.php
+++ p1/index.php
@@ -1,9 +1,26 @@
 <!doctype html>
 <html lang='en'>
-<head><title>Project 1</title>
+
+<head>
+    <title>Project 1: Rock Paper Scissors</title>
     <meta charset='utf-8'>
 </head>
+
 <body>
-    <h1>Project 1</h1>
+    <h1>Rock Paper Scissors!</h1>
+    <h2>How to Play</h2>
+    <ol>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ol>
+    <h2>Outcome</h2>
+    <ul>
+        <li>Player One threw </li>
+        <li>Player Two threw </li>
+        <li>And the winner is... </li>
+    </ul>
 </body>
+
 </html>
--- p1/rock-paper-scissors.php
+++ p1/rock-paper-scissors.php
@@ -1,1 +1,23 @@
 <?php
+$moves = ['rock', 'paper', 'scissors'];
+
+$move1 = $moves[rand(0, 2)];
+var_dump($move1);
+$move2 = $moves[rand(0, 2)];
+var_dump($move2);
+
+if ($move1 == $move2) {
+    var_dump("It's a tie!");
+} elseif ($move1 == $moves[0] and $move2 == $moves[1]) {
+    var_dump('Player Two wins!');
+} elseif ($move1 == $moves[0] and $move2 == $moves[2]) {
+    var_dump('Player One wins!');
+} elseif ($move1 == $moves[1] and $move2 == $moves[0]) {
+    var_dump('Player One wins!');
+} elseif ($move1 == $moves[1] and $move2 == $moves[2]) {
+    var_dump('Player Two wins!');
+} elseif ($move1 == $moves[2] and $move2 == $moves[0]) {
+    var_dump('Player Two wins!');
+} elseif ($move1 == $moves[2] and $move2 == $moves[1]) {
+    var_dump('Player One wins!');
+}
